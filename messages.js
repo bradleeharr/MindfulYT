20206b3d369f4f2f1852a97de690566dde842e98
Bradlee Harrison <56418392+bradleeharr@users.noreply.github.com>
2024-09-13 00:51:53 -0500

    Update messages.js - Replace hyphen with em dash

--- messages.js
+++ messages.js
@@ -7,9 +7,9 @@ window.messagesList = [
     "The canvas of life awaits your brushstroke. Paint a masterpiece of unparalleled achievement!",
     "Consider practicing mindfulness or meditation to relax and focus.",
     "This is a friendly reminder. It\'s been a while. Consider getting off YouTube and looking at something else when you get a chance.",
-    "I am as bad as the worst, but, thank God, I am as good as the best. -Walt Whitman",
-    "Peace is always beautiful. - Walt Whitman, Leaves of Grass",
-    "Nothing will work unless you do. -Maya Angelou"
+    "I am as bad as the worst, but, thank God, I am as good as the best. —Walt Whitman",
+    "Peace is always beautiful. —Walt Whitman, Leaves of Grass",
+    "Nothing will work unless you do. —Maya Angelou"
 
 
 ];
