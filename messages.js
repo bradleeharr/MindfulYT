ab61e3bea35558120d4b7e41ec4eba28824f2415
Bradlee Harrison <56418392+bradleeharr@users.noreply.github.com>
2024-11-03 01:45:23 -0600

    Update messages.js

--- messages.js
+++ messages.js
@@ -10,9 +10,9 @@ window.messagesList = [
     "I am as bad as the worst, but, thank God, I am as good as the best. —Walt Whitman",
     "Peace is always beautiful —Walt Whitman, Leaves of Grass",
     "Nothing will work unless you do. —Maya Angelou",
-    "Time is real, we can't rewind it. Out of everybody I met, who told the truth? Time did. —Common"
-    "I feel confident because I'm the best player in the world. It's simple. —LeBron James"
-    "I don't read books much. -LeBron James"
-    "Never say never, because limits, like fears, are often just an illusion. -Michael Jordan"
+    "Time is real, we can't rewind it. Out of everybody I met, who told the truth? Time did. —Common",
+    "I feel confident because I'm the best player in the world. It's simple. —LeBron James",
+    "I don't read books much. -LeBron James",
+    "Never say never, because limits, like fears, are often just an illusion. -Michael Jordan",
     "Do or do not. There is no try."
 ];
