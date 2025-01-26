1ad83abb6e20dc1f2daeead59f1b62a875dbccde
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-01-26 13:12:56 -0600

    Add linting using esLint and PrettierRc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+      globals: {
+        chrome: "readonly",
+        window: "readonly",
+        document: "readonly",
+        Constants: "writeable",
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
