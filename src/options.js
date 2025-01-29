8675c7e7e021a7fadd3385c0730a1f21a65c401c
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-01-28 20:58:25 -0600

    Update options to allow 'Enter' to work to save options

--- src/options.js
+++ src/options.js
@@ -1,79 +1,90 @@
 // options.js
 
-// When the DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Load existing config
-  chrome.storage.sync.get(
-    [
-      "minTime_m",
-      "minTime_s",
-      "maxTime_m",
-      "maxTime_s",
-      "blockShortsSet",
-      "newBlacklistedChannel",
-      "blacklistedChannels",
-    ],
-    (result) => {
-      console.log(result);
-      document.getElementById("minTime-m").value = result.minTime_m || "";
-      document.getElementById("minTime-s").value = result.minTime_s || "";
-      document.getElementById("maxTime-m").value = result.maxTime_m || "";
-      document.getElementById("maxTime-s").value = result.maxTime_s || "";
+    // Load existing config
+    chrome.storage.sync.get(
+        [
+            "minTime_m",
+            "minTime_s",
+            "maxTime_m",
+            "maxTime_s",
+            "blockShortsSet",
+            "newBlacklistedChannel",
+            "blacklistedChannels",
+        ],
+        (result) => {
+            console.log(result);
+            document.getElementById("minTime-m").value = result.minTime_m || "";
+            document.getElementById("minTime-s").value = result.minTime_s || "";
+            document.getElementById("maxTime-m").value = result.maxTime_m || "";
+            document.getElementById("maxTime-s").value = result.maxTime_s || "";
 
-      let blacklist = document.getElementById("blacklist");
+            let blacklist = document.getElementById("blacklist");
 
-      console.log(result.blacklistedChannels);
+            console.log(result.blacklistedChannels);
 
-      if (result.blacklistedChannels) {
-        result.blacklistedChannels.forEach((channelName) => {
-          const listItem = document.createElement("div");
-          listItem.className = "blacklist-item";
-          listItem.textContent = channelName;
-          blacklist.appendChild(listItem);
-        });
-      }
-    },
-  );
+            if (result.blacklistedChannels) {
+                result.blacklistedChannels.forEach((channelName) => {
+                    const listItem = document.createElement("div");
+                    listItem.className = "blacklist-item";
+                    listItem.textContent = channelName;
+                    blacklist.appendChild(listItem);
+                });
+            }
+        },
+    );
 
-  // Save button listener
-  document.getElementById("save").addEventListener("click", () => {
-    let minTime_m = parseInt(document.getElementById("minTime-m").value);
-    let minTime_s = parseInt(document.getElementById("minTime-s").value);
-    let maxTime_m = parseInt(document.getElementById("maxTime-m").value);
-    let maxTime_s = parseInt(document.getElementById("maxTime-s").value);
-    let blockShortsSet = document.getElementById("blockShortsSet").checked;
-    let newBlacklistedChannel = document.getElementById(
-      "newBlacklistedChannel",
-    ).value;
+    const saveButton = document.getElementById("save");
 
-    // Handle the list of blacklisted channels. We have to first get from chrome storage
-    // to then update
-    chrome.storage.sync.get(["blacklistedChannels"], (result) => {
-      let blacklistedChannels = result.blacklistedChannels || [];
-
-      console.log("newBlackListedChannel: ", newBlacklistedChannel);
-      console.log("blacklistedChannels: ", blacklistedChannels);
-      blacklistedChannels.push(newBlacklistedChannel);
-
-      // Save the configuration
-      chrome.storage.sync.set({
-        minTime_m,
-        minTime_s,
-        maxTime_m,
-        maxTime_s,
-        blockShortsSet,
-        blacklistedChannels,
-      });
+    // Allow enter to work to hit the save button anywhere
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            console.log("EVENT LOGGED");
+            event.preventDefault();
+            saveButton.click();
+        }
     });
 
-    // Show feedback message
-    const feedbackElement = document.getElementById("feedback");
-    feedbackElement.textContent = "Settings saved!";
-    feedbackElement.style.display = "inline";
+    saveButton.addEventListener("click", saveSettings);
 
-    // Hide feedback message after 3 seconds
-    setTimeout(() => {
-      feedbackElement.style.display = "none";
-    }, Constants.FEEDBACK_MESSAGE_TIMEOUT);
-  });
+    function saveSettings() {
+        let minTime_m = parseInt(document.getElementById("minTime-m").value);
+        let minTime_s = parseInt(document.getElementById("minTime-s").value);
+        let maxTime_m = parseInt(document.getElementById("maxTime-m").value);
+        let maxTime_s = parseInt(document.getElementById("maxTime-s").value);
+        let blockShortsSet = document.getElementById("blockShortsSet").checked;
+        let newBlacklistedChannel = document.getElementById(
+            "newBlacklistedChannel",
+        ).value;
+
+        // Handle the list of blacklisted channels. We have to first get from chrome storage
+        // to then update
+        chrome.storage.sync.get(["blacklistedChannels"], (result) => {
+            let blacklistedChannels = result.blacklistedChannels || [];
+
+            console.log("newBlackListedChannel: ", newBlacklistedChannel);
+            console.log("blacklistedChannels: ", blacklistedChannels);
+            blacklistedChannels.push(newBlacklistedChannel);
+
+            // Save the configuration
+            chrome.storage.sync.set({
+                minTime_m,
+                minTime_s,
+                maxTime_m,
+                maxTime_s,
+                blockShortsSet,
+                blacklistedChannels,
+            });
+        });
+
+        // Show feedback message
+        const feedbackElement = document.getElementById("feedback");
+        feedbackElement.textContent = "Settings saved!";
+        feedbackElement.style.display = "inline";
+
+        // Hide feedback message after 3 seconds
+        setTimeout(() => {
+            feedbackElement.style.display = "none";
+        }, Constants.FEEDBACK_MESSAGE_TIMEOUT);
+    }
 });
